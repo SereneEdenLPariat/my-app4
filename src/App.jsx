98aae44e1ffe8fdb9b1cb37b7be146e6df91b639
SereneEdenLPariat <serene.pariat@sonicbolt>
2022-07-25 17:45:12 +0530

    yjmum

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import UseHooks from "./UseHooks.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <UseHooks />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UseHooks.jsx
@@ -0,0 +1,86 @@
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  createContext,
+  useContext,
+} from "react";
+
+// preserves value
+// DOES NOT trigger re-render
+// target DOM nodes/elements
+
+const UserContext = createContext();
+
+const UseHooks = () => {
+  const [user, setUser] = useState("PLEASE SUBMIT");
+
+  const refName = useRef(null);
+  const refEmail = useRef(null);
+  const refPassword = useRef(null);
+
+  const handleSubmit = (e) => {
+    //console.log(refContainer.current);
+    e.preventDefault();
+    setUser("THANK YOU");
+  };
+
+  useEffect(() => {});
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <h1 id="h1">FORM</h1>
+          <br></br>
+          <label id="name-label" htmlFor="inputName">
+            Name:{" "}
+          </label>
+          <input type="text" ref={refName} />
+        </div>
+        <div>
+          <label id="email-label" htmlFor="inputEmail">
+            Email:{" "}
+          </label>
+          <input type="text" ref={refEmail} />
+        </div>
+        <div>
+          <label id="password-label" htmlFor="inputPassword">
+            Password:{" "}
+          </label>
+          <input type="password" ref={refPassword} />
+        </div>
+        <br></br>
+        <button id="submit-label" type="submit">
+          Submit
+        </button>
+        <UserContext.Provider value={user}>
+          <Component1 />
+        </UserContext.Provider>
+      </form>
+    </>
+  );
+};
+
+const Component1 = () => {
+  return (
+    <>
+      <Component2 />
+    </>
+  );
+};
+
+const Component2 = () => {
+  return (
+    <>
+      <Component3 />
+    </>
+  );
+};
+
+const Component3 = () => {
+  const user = useContext(UserContext);
+  return <p id="user">{user}</p>;
+};
+
+export default UseHooks;
